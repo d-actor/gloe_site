f75558de0a70513b053c46f65d1c28d893703702
Dan Actor <danltactor@gmail.com>
2018-09-13 16:26:36 -0600

    one more

--- src/components/header.js
+++ src/components/header.js
@@ -6,44 +6,88 @@ import {
 } from 'semantic-ui-react';
 import Logo from "../assets/images/logo.jpg";
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'black',
-      marginBottom: '1.45rem',
-      display: 'flex',
-      height: '4em',
-    }}
-  >
-    <Menu.Menu>
-      <img src={Logo} style={styles.logo} />
-    </Menu.Menu>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
+class Header extends React.Component {
+  navs = () => {
+    return (
+      <Menu.Menu position='right' style={{color: 'white'}}>
+        <Link to='/contact/'>
+          <Menu.Item style={{color: 'white'}} name='Contact' />
         </Link>
-      </h1>
-    </div>
-    <Menu.Menu style={{flex: 'right', display: 'flex', flexDirection: 'column'}}position="right">
-      <Link to="/contact/">
-        <Menu.Item name='Contact' style={styles.navText} />
-      </Link>
-    </Menu.Menu>
-  </div>
-)
+        <Link to='/music/'>
+          <Menu.Item name='Music' style={{color: 'white'}} />
+        </Link>
+      </Menu.Menu>
+    );
+  }
+
+  render() {
+    const { siteTitle } = this.props;
+    return (
+      <div
+        style={{
+          background: 'black',
+        }}
+      >
+        <Menu pointing secondary>
+          <Menu.Menu>
+            <img src={Logo} style={styles.logo} />
+          </Menu.Menu>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+          { this.navs() }
+        </Menu>
+      </div>
+      )
+  }
+}
+
+//const Header = ({ siteTitle }) => (
+//  <div
+//    style={{
+//      background: 'black',
+//      marginBottom: '1.45rem',
+//      display: 'flex',
+//      height: '4em',
+//    }}
+//  >
+//    <Menu.Menu>
+//      <img src={Logo} style={styles.logo} />
+//    </Menu.Menu>
+//    <div
+//      style={{
+//        margin: '0 auto',
+//        maxWidth: 960,
+//        padding: '1.45rem 1.0875rem',
+//      }}
+//    >
+//      <h1 style={{ margin: 0 }}>
+//        <Link
+//          to="/"
+//          style={{
+//            color: 'white',
+//            textDecoration: 'none',
+//          }}
+//        >
+//          {siteTitle}
+//        </Link>
+//      </h1>
+//    </div>
+//    <Menu.Menu style={{flex: 'right'}}position="right">
+//      <Link to="/contact/">
+//        <Menu.Item name='Contact' style={styles.navText} />
+//      </Link>
+//    </Menu.Menu>
+//  </div>
+//)
 
 const styles = {
   logo: {
