457f181e161ce6ff2bfa8087658631232404b229
Dan Actor <danltactor@gmail.com>
2018-09-14 16:12:06 -0600

    add spotify embed

--- src/pages/music.js
+++ src/pages/music.js
@@ -9,6 +9,7 @@ class MusicPage extends React.Component {
     return(
       <div>
         Music
+        <iframe src="https://open.spotify.com/embed/artist/3WM5LP74A1mISkxx0L6kcT" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
     );
   }
