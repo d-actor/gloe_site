8d9c86f87809fc5cfc048dad1a177e4e0f8b3a1b
Dan Actor <danltactor@gmail.com>
2018-09-15 16:39:54 -0600

    background color

---
+++ src/html.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import favicon from './layouts/favicon.ico';
+
+let inlinedStyles = "";
+if (process.env.NODE_ENV === "production") {
+  try {
+    inlinedStyles = require("!raw-loader!../public/styles.css");
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+class HTML extends React.Component {
+  render() {
+    let css;
+    if (process.env.NODE_ENV === "production") {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: inlinedStyles }}
+        />
+      );
+    }
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          {this.props.headComponents}
+          <link rel="shortcut icon" href={favicon} />
+          {css}
+        </head>
+        <body style={styles.background}>
+          <div id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    );
+  }
+}
+
+const styles = {
+  background: {
+    background: "#222222",
+  },
+}
+
+export default HTML;
+
